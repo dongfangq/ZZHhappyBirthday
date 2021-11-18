78cc56e4681e240830e0b990f195a28379689ff6
dongfangq <49439328+dongfangq@users.noreply.github.com>
2021-11-18 15:50:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3|董|小|姐|发|来|贺|电|祝|大|帅|哥|张|振|虎|生|日|快|乐|#rectangle|');
+      S.UI.simulate('|#countdown 3|董|小|姐|发|来|贺|电|祝|小|帅|哥|张|振|虎|生|日|快|乐|#rectangle|');
     }
 
     S.Drawing.loop(function () {
