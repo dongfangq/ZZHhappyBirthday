d1ba4ed21fb8609eff62758ca01b22337649dea1
dongfangq <49439328+dongfangq@users.noreply.github.com>
2022-08-05 12:47:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-     S.UI.simulate('|#countdown 3|哈|#rectangle|');
+     S.UI.simulate('|#countdown 3|.|#rectangle|');
     }
 
     S.Drawing.loop(function () {
